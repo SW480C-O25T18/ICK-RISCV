b7d42cdb72e853d6aae2ba9cabd4ab493fb2d619
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2025-03-01 20:33:37 -0500

    fixing more AUnit syntax issues

--- source/tests/riscv64/core_context_tests.ads
+++ source/tests/riscv64/core_context_tests.ads
@@ -3,6 +3,7 @@ with AUnit.Test_Cases; use AUnit.Test_Cases;
 
 package Core_Context_Tests is
    type Core_Context_Test is new Test_Cases.Test_Case with null record;
+   Test_Case : aliased Core_Context_Test := new Core_Context_Test'(Name => "Core_Context_Tests");
    procedure Register_Tests (T : in out Core_Context_Test);
    function Name (T : Core_Context_Test) return Message_String;
 end Core_Context_Tests;
--- source/tests/riscv64/gp_context_tests.ads
+++ source/tests/riscv64/gp_context_tests.ads
@@ -3,6 +3,7 @@ with AUnit.Test_Cases; use AUnit.Test_Cases;
 
 package GP_Context_Tests is
    type GP_Context_Test is new Test_Cases.Test_Case with null record;
+   Test_Case : aliased GP_Context_Test := new GP_Context_Test'(Name => "GP_Context_Tests");
    procedure Register_Tests (T : in out GP_Context_Test);
    function Name (T : GP_Context_Test) return Message_String;
 end GP_Context_Tests;
