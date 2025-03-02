4d1cd51928b289b0ed63a5d6066a0b6b998aea3c
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2025-03-01 19:25:49 -0500

    correcting AUnit files.

--- source/tests/riscv64/Context_Test_Suite.adb
+++ source/tests/riscv64/Context_Test_Suite.adb
@@ -3,19 +3,19 @@ with CSR_Extension_Tests;
 with Core_Context_Tests;
 with FP_Context_Tests;
 with GP_Context_Tests;
+with Success_Fork_Result_Tests;
 
 package body Context_Test_Suite is
-   use AUnit.Test_Suites;
 
-   Result : aliased Test_Suite;
-
-   function Suite return Access_Test_Suite is
+   function Suite return AUnit.Test_Suites.Access_Test_Suite is
+      Result : AUnit.Test_Suites.Access_Test_Suite := AUnit.Test_Suites.New_Suite;
    begin
-      Add_Test (Result'Access, CSR_Extension_Tests.Test_Case'Access);
-      Add_Test (Result'Access, Core_Context_Tests.Test_Case'Access);
-      Add_Test (Result'Access, FP_Context_Tests.Test_Case'Access);
-      Add_Test (Result'Access, GP_Context_Tests.Test_Case'Access);
-      Add_Test (Result'Access, Success_Fork_Result_Tests.Test_Case'Access);
-      return Result'Access;
+      Result.Add_Test (GP_Context_Tests.Test_Case'Access);
+      Result.Add_Test (CSR_Extension_Tests.Test_Case'Access);
+      Result.Add_Test (FP_Context_Tests.Test_Case'Access);
+      Result.Add_Test (Core_Context_Tests.Test_Case'Access);
+      Result.Add_Test (Success_Fork_Result_Tests.Test_Case'Access);
+      return Result;
    end Suite;
+
 end Context_Test_Suite;
--- source/tests/riscv64/arch_rv64_test_suite.ads
+++ source/tests/riscv64/arch_rv64_test_suite.ads
@@ -1,7 +1,5 @@
--- Composition package:
-with AUnit; use AUnit;
-with AUnit.Test_Suites; use AUnit.Test_Suites;
+with AUnit.Test_Suites;
 
 package Arch_RV64_Test_Suite is
-   function Suite return Test_Suites.Access_Test_Suite;
+   function Suite return AUnit.Test_Suites.Access_Test_Suite;
 end Arch_RV64_Test_Suite;
