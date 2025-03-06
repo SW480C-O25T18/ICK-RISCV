4e33473b1cc257164108fb7fa8bd9076bb78925e
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2025-03-06 08:43:52 -0500

    still trying to fix stubs package

--- source/tests/riscv64/stubs.adb
+++ source/tests/riscv64/stubs.adb
@@ -7,18 +7,22 @@ package body Stubs is
    procedure Initialize_Core_Locals is
    begin
       for I in Core_Locals'Range loop
-         -- Convert the numeric value to an address using To_Address.
-         Core_Locals(I).User_Stack := To_Address(16#1000# + I * 16#100#);
-         -- With "use Interfaces;" the conversion operator for Unsigned_64 is now visible.
-         Core_Locals(I).Hart_ID    := Unsigned_64(I - 1);
-         Core_Locals(I).Number     := I;
+         declare
+            -- Convert the integer expression to type Integer_Address.
+            A : Integer_Address := Integer_Address(16#1000# + I * 16#100#);
+         begin
+            Core_Locals(I).User_Stack := To_Address(A);
+         end;
+         -- Explicitly convert to Unsigned_64.
+         Core_Locals(I).Hart_ID := Unsigned_64(I - 1);
+         Core_Locals(I).Number  := I;
       end loop;
    end Initialize_Core_Locals;
 
    function Has_Extension(Bit : Unsigned_64) return Boolean is
    begin
-      -- For testing, assume that F (0x20) and D (0x40) extensions are supported.
-      if Bit = 16#20# or Bit = 16#40# then
+      -- Convert numeric literals to Unsigned_64 to match the type of Bit.
+      if Bit = Unsigned_64(16#20#) or Bit = Unsigned_64(16#40#) then
          return True;
       else
          return False;
