a35a94a6634250828ff963a35f8837f4d872e5c5
Sean C. Weeks <112424278+scweeks@users.noreply.github.com>
2025-03-06 08:39:28 -0500

    fixing errors with stubs.ads and adb

--- source/tests/riscv64/stubs.adb
+++ source/tests/riscv64/stubs.adb
@@ -1,17 +1,21 @@
 with Interfaces;
 with System;
+with System.Storage_Elements; use System.Storage_Elements;
+
 package body Stubs is
 
    procedure Initialize_Core_Locals is
    begin
       for I in Core_Locals'Range loop
-         Core_Locals(I).User_Stack := System.Address'Value(16#1000# + I * 16#100#);
-         Core_Locals(I).Hart_ID    := Interfaces.Unsigned_64(I - 1);
+         -- Convert the numeric value to an address using To_Address.
+         Core_Locals(I).User_Stack := To_Address(16#1000# + I * 16#100#);
+         -- With "use Interfaces;" the conversion operator for Unsigned_64 is now visible.
+         Core_Locals(I).Hart_ID    := Unsigned_64(I - 1);
          Core_Locals(I).Number     := I;
       end loop;
    end Initialize_Core_Locals;
 
-   function Has_Extension(Bit : Interfaces.Unsigned_64) return Boolean is
+   function Has_Extension(Bit : Unsigned_64) return Boolean is
    begin
       -- For testing, assume that F (0x20) and D (0x40) extensions are supported.
       if Bit = 16#20# or Bit = 16#40# then
